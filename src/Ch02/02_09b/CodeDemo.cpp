32422bb5ab02808e962f205c91866ed5d3fd0185
fguarato <61125633+fguarato@users.noreply.github.com>
2025-11-05 17:09:05 +0000

    CH2_9 completed

--- src/Ch02/02_09b/CodeDemo.cpp
+++ src/Ch02/02_09b/CodeDemo.cpp
@@ -7,7 +7,19 @@
 
 enum class character_role {protagonist, antagonist, sidekick, npc};
 
+struct game_character {
+    std::string character_name;
+    int level;
+    character_role role;
+};
+
 int main(){
+    game_character buddy;
+    buddy.character_name = "Tony";
+    buddy.level = 10;
+    buddy.role = character_role::protagonist;
+
+    std::cout << buddy.character_name << " is a level " << buddy.level << " with role " << int(buddy.role) << std::endl;
     
     std::cout << std::endl << std::endl;
     return 0;
