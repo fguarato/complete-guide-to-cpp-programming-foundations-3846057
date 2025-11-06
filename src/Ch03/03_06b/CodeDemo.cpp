adc427e0071d27c6a4e56b37513767aa9f690321
fguarato <61125633+fguarato@users.noreply.github.com>
2025-11-06 22:16:47 +0000

    CH3_6 completed

--- src/Ch03/03_06b/CodeDemo.cpp
+++ src/Ch03/03_06b/CodeDemo.cpp
@@ -2,12 +2,19 @@
 // Exercise 03_06
 // Preprocessor Directives, by Eduardo Corpeño 
 
-#include <iostream>
-#include <cstdint>
+#include <iostream> // preprocessor directive
+#include <cstdint> // preprocessor directive
+
+#define MAX_AMMO 500
+#define DEBUG
 
 int main(){
     int32_t ammo = 100;
     uint8_t health_items = 5;
+
+#ifdef DEBUG // MACRO
+    std::cout << "[DEBUG] Starting game" << std::endl;
+#endif
 
     ammo += 200; // Player finds extra ammo
     health_items -= 2; // Player uses some health items
