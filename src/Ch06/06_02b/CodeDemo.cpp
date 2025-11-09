9cbdc63006331784e65f1a32687372eb05758c5d
fguarato <61125633+fguarato@users.noreply.github.com>
2025-11-09 19:37:54 +0000

    CH6_02 addresses in swap are defined before

--- src/Ch06/06_02b/CodeDemo.cpp
+++ src/Ch06/06_02b/CodeDemo.cpp
@@ -4,14 +4,29 @@
 
 #include <iostream>
 
+// takes arguments by value
+int square(int x){
+    return x*x;
+}
+
+// Takes argument by address
+void swap(int* x, int* y){
+    int temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
 int main(){
     int a = 9, b;
-    // TODO: square
+    b = square(a);
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    int* x = &a;
+    int* y = &b;
+    swap(x, y);
     std::cout << "a = " << a << ", b = " << b << std::endl;
     // TODO: swap
-    std::cout << "a = " << a << ", b = " << b << std::endl;
-    // TODO: swap
-    std::cout << "a = " << a << ", b = " << b << std::endl;
+    //std::cout << "a = " << a << ", b = " << b << std::endl;
     
     std::cout << std::endl << std::endl;
     return 0;
