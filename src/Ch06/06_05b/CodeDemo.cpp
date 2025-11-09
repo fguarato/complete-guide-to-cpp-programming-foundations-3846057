bb381c2fca2177f93443644f0382da3bd84cb719
fguarato <61125633+fguarato@users.noreply.github.com>
2025-11-09 23:48:43 +0000

    CH6_05 completed

--- src/Ch06/06_05b/CodeDemo.cpp
+++ src/Ch06/06_05b/CodeDemo.cpp
@@ -10,11 +10,20 @@ int getSpeed(int distance, int time){
     return distance / time;
 }
 
+int getSpeed(const double& time){
+    return static_cast<int>(200.0 / time);
+}
+
 int main(){
     int speedInt;
 
     speedInt = getSpeed(200, 4);
     std::cout << "Speed (int): " << speedInt << std::endl;
+
+    double speedOverload;
+
+    speedOverload = getSpeed(2.0);
+    std::cout << "Speed (overload): " << speedOverload << std::endl;
     
     std::cout << std::endl << std::endl;
     return 0;
