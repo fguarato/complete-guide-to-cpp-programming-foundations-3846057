0312434dff781e1ba0b6dd1389e11400e4b42a35
fguarato <61125633+fguarato@users.noreply.github.com>
2025-11-09 20:03:55 +0000

    CH6_02 completed

--- src/Ch06/06_02b/CodeDemo.cpp
+++ src/Ch06/06_02b/CodeDemo.cpp
@@ -17,14 +17,21 @@ void swap(int* x, int* y){
     *y = temp;
 }
 
+// Takes values by reference
+void swap(int& x, int& y){
+    int temp = x;
+    x = y;
+    y = temp;
+}
+
 int main(){
     int a = 9, b;
     b = square(a);
     std::cout << "a = " << a << ", b = " << b << std::endl;
     swap(&a, &b);
     std::cout << "a = " << a << ", b = " << b << std::endl;
-    // TODO: swap
-    //std::cout << "a = " << a << ", b = " << b << std::endl;
+    swap(a, b);
+    std::cout << "a = " << a << ", b = " << b << std::endl;
     
     std::cout << std::endl << std::endl;
     return 0;
