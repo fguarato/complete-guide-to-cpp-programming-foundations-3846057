801ff627c1162b96f1ba3490bbbadba5198e5571
fguarato <61125633+fguarato@users.noreply.github.com>
2025-11-10 12:08:27 +0000

    CH4_06 completed

--- src/Ch04/04_06b/CodeDemo.cpp
+++ src/Ch04/04_06b/CodeDemo.cpp
@@ -2,10 +2,32 @@
 // Exercise 04_06
 // The Vector Class, by Eduardo Corpeño 
 
+#include <vector>
 #include <iostream>
 #include <string>
 
 int main(){
+    std::vector<std::string> checkpoint = {"Start", "Forest", "Castle"};
+
+    // Print size of the vector
+    std::cout << checkpoint.size() << std::endl;
+    std::cout << std::endl;
+
+    checkpoint.push_back("Cave");
+    checkpoint.push_back("Finish");
+
+    for(auto x : checkpoint){
+        std::cout << x << std::endl;
+    }
+    // Print out size of vector
+    std::cout << "The game has " << checkpoint.size() << " checkpoints." << std::endl;
+
+    // Update elements
+    checkpoint[2] = "Dark castle";
+
+    for(auto x : checkpoint){
+        std::cout << x << std::endl;
+    }
     
     std::cout << std::endl << std::endl;
     return 0;
