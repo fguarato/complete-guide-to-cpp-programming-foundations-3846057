9fe20da35c71e3f6fd4de049d7579e22df4cb1fa
fguarato <61125633+fguarato@users.noreply.github.com>
2025-11-11 21:51:20 +0000

    CH4_10 completed

--- src/Ch04/04_10b/CodeDemo.cpp
+++ src/Ch04/04_10b/CodeDemo.cpp
@@ -3,26 +3,37 @@
 // The String Class, by Eduardo Corpeño 
 
 #include <iostream>
-#include <cstring>
+#include <string>
+#include <vector>
 
 int main(){
-    const size_t LENGTH = 50;
-    char racer1[] = "Speedy";
-    char racer2[LENGTH];
-    char raceResult[LENGTH * 2];
-
-    strncpy(racer2, "Lightning", sizeof(racer2) - 1);
-    racer2[sizeof(racer2) - 1] = '\0';
+    std::string racer1 = "Speedy";
+    std::string racer2 = "Lightning";
+    std::string raceResult;
 
     std::cout << "Racer 1: " << racer1 << std::endl;
     std::cout << "Racer 2: " << racer2 << std::endl;
 
-    strncpy(raceResult, "And the winner is ", sizeof(raceResult) - 1);
-    raceResult[sizeof(raceResult) - 1] = '\0';
-
-    strncat(raceResult, racer1, sizeof(raceResult) - strlen(raceResult) - 1);
+    raceResult = "The winner is: " + racer1;
 
     std::cout << raceResult << std::endl;
+
+    std::vector<int>* v1 = new std::vector<int> ({1, 2, 3});
+    std::vector<int>* v2;
+    v2=v1;
+    v1->push_back(4);
+    v2->push_back(5);
+
+    // for(size_t x = 0; x<v1->size(); x++){
+    //     // std::cout << v1->at(x) << std::endl;
+    //     std::cout << (*v1).at(x) << std::endl;
+    // }
+    // std::cout << std::endl;
+
+    for(auto y : *v2){
+        std::cout << y << std::endl;
+    }
+    std::cout << std::endl;
 
     std::cout << std::endl << std::endl;
     return 0;
